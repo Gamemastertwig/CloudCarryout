f93ea50d370f5f948f86aaf3ec443ea3b03e6aaa
Gamemastertwig <brandonlocker@hotmail.com>
2020-06-16 17:32:21 -0500

    added lamda (go) to trigger aws-batch

---
+++ lamdaFuncs/trigger-batch/trigger-batch.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/batch"
+)
+
+func main() {
+	lambda.Start(Handler)
+}
+
+//Handler request from Lambda
+func Handler(request map[string]interface{}) (string, error) {
+
+	svc := batch.New(session.New())
+	input := &batch.SubmitJobInput{
+		JobDefinition: aws.String("cloudcarryout-batch-job-test"),
+		JobName:       aws.String("testJob"),
+		JobQueue:      aws.String("cloudcarryout-batch-job-queue"),
+	}
+
+	results, err := svc.SubmitJob(input)
+	if err != nil {
+		fmt.Println(err.Error())
+		return "", err
+	}
+
+	fmt.Println(results)
+	return fmt.Sprintln(results), nil
+}
