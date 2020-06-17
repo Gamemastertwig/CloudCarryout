0bbad73b58355612d3f17ebd3ab9d3df76abe8d7
NehemiahG7 <nehemiahgene@gmail.com>
2020-06-16 19:23:04 -0500

    added temp .tf files

--- Terraform/webApp/loadBalancer.tf
+++ Terraform/webApp/loadBalancer.tf
@@ -1,20 +1,31 @@
+resource "aws_vpc" "vpc" {
+  cidr_block = "170.170.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "vpc"
+  }
+}
+
+resource "aws_subnet" "main"
 resource "aws_lb" "webAppLb"{
   name = "webAppLb"
   internal = false
   load_balancer_type = "application"
-  security_groups = 
+  security_groups = ["${aws_security_group.traffic.name}"]
+  subnets = []
 }
 
 resource "aws_lb_target_group" "ec2Target"{
     name = "ec2Target"
     port = "80"
     protocol = "HTTP"
-    vpc_id = "${aws_defaul_vpc.vpc.id}"
+    vpc_id = "${aws_default_vpc.cloud.id}"
 }
 
 resource "aws_lb_target_group_attachment" "ec2Attachment" {
     target_group_arn = "${aws_lb_target_group.ec2Target.arn}"
-    target_id = "$aws_instance.web_server.id"
+    target_id = "${aws_instance.web_server.id}"
     port = 80
 }
 
@@ -29,8 +40,23 @@ resource "aws_lb_listener" "appListener"{
   }
 }
 
-resource "aws_default_vpc" "vpc" {
-  tags = {
-    Name = "vpc"
+resource "aws_security_group" "traffic" {
+  description = "Allow traffic on port 80"
+
+  ingress {
+    from_port   = 0 
+    to_port     = 0
+    protocol =   "-1"
+
+    cidr_blocks =  ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
   }
 }
+
+
