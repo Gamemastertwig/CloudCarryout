716112dd72af20c686d238d40dd5e79fe44a2def
Gamemastertwig <brandonlocker@hotmail.com>
2020-06-16 20:11:14 -0500

    updated setup.sh

--- Docker/setup.sh
+++ Docker/setup.sh
@@ -3,9 +3,14 @@
 aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
 aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
 
+# temp for testing
+# AWS_BUCKET="brandonlocker-test"
+# FILE_TO_WATCH="jobs/job.json"
+
 # code to determine which script to run
 # get json from s3 bucket
-aws s3 cp s3://brandonlocker-test/jobs/job.json .
+aws s3 cp s3://${AWS_BUCKET}/${FILE_TO_WATCH} .
+
 if [[ $? -eq 0 ]]
 then
     echo "Job found!"
@@ -17,8 +22,12 @@ fi
 
 # function for initing terraform and applying it
 func_terraform() {
+    # folder stores the desired folder ($1 argument passed to func_terraform)
+    folder=$1
+    cd terraform/${folder}
     terraform init
     terraform apply -auto-approve
+    exit 0
 }
 
 # webApp
@@ -26,19 +35,15 @@ grep -q "\"webApp\"" job.json
 if [[ $? -eq 0 ]]
 then
     echo "Provisioning webApp resources..."
-    cd terraform/webApp
-    func_terraform
-    exit 0
+    func_terraform webApp
 fi
 
 # windowsApp
-grep -q "\"windowApp\"" job.json
+grep -q "\"windowsApp\"" job.json
 if [[ $? -eq 0 ]]
 then
-    echo "Provisioning windowApp resources..."
-    cd terraform/testing
-    func_terraform
-    exit 0
+    echo "Provisioning windowsApp resources..."
+    func_terraform windowsApp
 fi
 
 # msmq
@@ -46,6 +51,7 @@ grep -q "\"msmq\"" job.json
 if [[ $? -eq 0 ]]
 then
     echo "Provisioning msmq resources..."
+    func_terraform msmq
 fi
 
 # no valid selection
